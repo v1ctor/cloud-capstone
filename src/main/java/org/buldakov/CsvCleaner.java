3b909ea447d464d122c84c5dcebe4880042e3144
Victor Buldakov <victor@buldakov.org>
2016-01-23 18:58:59 +0300

    fix canceled

--- src/main/java/org/buldakov/CsvCleaner.java
+++ src/main/java/org/buldakov/CsvCleaner.java
@@ -98,7 +98,7 @@ public class CsvCleaner {
                 // 72 - "Div2LongestGTime",
                 // 73 - "Div2WheelsOff",
                 // 74 - "Div2TailNum",
-                if (!"1".equals(nextLine[41])) { //Cancelled
+                if (nextLine[41].isEmpty()) { //Cancelled
                     writer.writeNext(result.toArray(new String[0]));
                 }
 
