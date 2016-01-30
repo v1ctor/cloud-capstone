5661205c86ff76855c295e104a86dc4687b56cf8
Victor Buldakov <victor@buldakov.org>
2016-01-30 12:27:48 +0300

    change input format

--- src/main/java/org/buldakov/task2_1/JobRunner.java
+++ src/main/java/org/buldakov/task2_1/JobRunner.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.io.BooleanWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.buldakov.common.TextArrayWritable;
@@ -41,19 +42,17 @@ public class JobRunner {
 
         Job top10AirlinePerformance = Job.getInstance(conf, "Top Departure Carrier performance");
 
-        top10AirlinePerformance.setOutputKeyClass(Text.class);
-        top10AirlinePerformance.setOutputValueClass(TextArrayWritable.class);
-
+        FileInputFormat.setInputPaths(top10AirlinePerformance, tmpPath);
+        top10AirlinePerformance.setInputFormatClass(KeyValueTextInputFormat.class);
+        top10AirlinePerformance.setMapperClass(TopCarriersByDepPerformanceMapper.class);
         top10AirlinePerformance.setMapOutputKeyClass(Text.class);
         top10AirlinePerformance.setMapOutputValueClass(TextArrayWritable.class);
 
-        top10AirlinePerformance.setMapperClass(TopCarriersByDepPerformanceMapper.class);
         top10AirlinePerformance.setReducerClass(TopCarriersDepByPerformanceReducer.class);
-
-        FileInputFormat.setInputPaths(top10AirlinePerformance, tmpPath);
-        FileOutputFormat.setOutputPath(top10AirlinePerformance, resultPath);
-
+        top10AirlinePerformance.setOutputKeyClass(Text.class);
+        top10AirlinePerformance.setOutputValueClass(TextArrayWritable.class);
         top10AirlinePerformance.setOutputFormatClass(TextOutputFormat.class);
+        FileOutputFormat.setOutputPath(top10AirlinePerformance, resultPath);
 
         top10AirlinePerformance.setJarByClass(JobRunner.class);
         System.exit(top10AirlinePerformance.waitForCompletion(true) ? 0 : 1);
---
+++ src/main/java/org/buldakov/task2_3/JobRunner.java
@@ -0,0 +1,8 @@
+package org.buldakov.task2_3;
+
+public class JobRunner {
+
+    public static void main(String[] args) {
+
+    }
+}
