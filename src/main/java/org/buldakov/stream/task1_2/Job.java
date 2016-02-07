4601852501d5bfc45892daf90102318a79de1ebb
Victor Buldakov <victor@buldakov.org>
2016-02-07 15:23:47 +0300

    not use apply

--- src/main/java/org/buldakov/stream/task1_2/Job.java
+++ src/main/java/org/buldakov/stream/task1_2/Job.java
@@ -29,12 +29,12 @@ public class Job {
                 System.out.println(value._1 + " " + value._2);
                 OnTimeRow row = OnTimeRow.parse(value._2);
                 int late = row.isLateArrival() ? 1 : 0;
-                return Tuple2.apply(row.getUniqueCarrier(), Tuple2.apply(1, late));
+                return new Tuple2<>(row.getUniqueCarrier(), new Tuple2<>(1, late));
             }
         }).reduceByKey(new Function2<Tuple2<Integer, Integer>, Tuple2<Integer, Integer>, Tuple2<Integer, Integer>>() {
             @Override
             public Tuple2<Integer, Integer> call(Tuple2<Integer, Integer> first, Tuple2<Integer, Integer> second) throws Exception {
-                return Tuple2.apply(first._1 + second._1, first._2 + second._2);
+                return new Tuple2<>(first._1 + second._1, first._2 + second._2);
             }
         }).mapToPair(new PairFunction<Tuple2<String, Tuple2<Integer, Integer>>, String, Double>() {
             @Override
@@ -42,7 +42,7 @@ public class Job {
                 double count = value._2._1;
                 double late = value._2._2;
                 double percentOnTime = 100.0 - (late / count * 100.0);
-                return Tuple2.apply(value._1, percentOnTime);
+                return new Tuple2<>(value._1, percentOnTime);
             }
         }).mapToPair(new PairFunction<Tuple2<String, Double>, Double, String>() {
             @Override
