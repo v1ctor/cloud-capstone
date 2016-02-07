2fc86474e6bac8e816530291a25a2be5d1c2f164
Victor Buldakov <victor@buldakov.org>
2016-02-08 00:18:47 +0300

    realise sliding percentage

--- src/main/java/org/buldakov/stream/task1_2/Job.java
+++ src/main/java/org/buldakov/stream/task1_2/Job.java
@@ -23,25 +23,24 @@ public class Job {
         JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
 
         KafkaUtils.createStream(jssc, "localhost", "spark", Collections.singletonMap("ontime_performance", 1))
-            .mapToPair(new PairFunction<Tuple2<String, String>, String, Tuple2<Integer, Integer>>() {
+            .mapToPair(new PairFunction<Tuple2<String, String>, String, Tuple2<Integer, Double>>() {
             @Override
-            public Tuple2<String, Tuple2<Integer, Integer>> call(Tuple2<String, String> value) throws Exception {
+            public Tuple2<String, Tuple2<Integer, Double>> call(Tuple2<String, String> value) throws Exception {
                 OnTimeRow row = OnTimeRow.parse(value._2);
-                int late = row.isLateArrival() ? 1 : 0;
-                return new Tuple2<>(row.getUniqueCarrier(), new Tuple2<>(1, late));
+                double percent = row.isLateArrival() ? 0.0 : 100.0;
+                return new Tuple2<>(row.getUniqueCarrier(), new Tuple2<>(1, percent));
             }
-        }).reduceByKey(new Function2<Tuple2<Integer, Integer>, Tuple2<Integer, Integer>, Tuple2<Integer, Integer>>() {
+        }).reduceByKey(new Function2<Tuple2<Integer, Double>, Tuple2<Integer, Double>, Tuple2<Integer, Double>>() {
             @Override
-            public Tuple2<Integer, Integer> call(Tuple2<Integer, Integer> first, Tuple2<Integer, Integer> second) throws Exception {
-                return new Tuple2<>(first._1 + second._1, first._2 + second._2);
+            public Tuple2<Integer, Double> call(Tuple2<Integer, Double> first, Tuple2<Integer, Double> second) throws Exception {
+                int count = first._1 + second._1;
+                double percent = (first._1 * first._2 + second._1 * second._2) / count;
+                return new Tuple2<>(count, percent);
             }
-        }).mapToPair(new PairFunction<Tuple2<String, Tuple2<Integer, Integer>>, String, Double>() {
+        }).mapToPair(new PairFunction<Tuple2<String, Tuple2<Integer, Double>>, String, Double>() {
             @Override
-            public Tuple2<String, Double> call(Tuple2<String, Tuple2<Integer, Integer>> value) throws Exception {
-                double count = value._2._1;
-                double late = value._2._2;
-                double percentOnTime = 100.0 - (late / count * 100.0);
-                return new Tuple2<>(value._1, percentOnTime);
+            public Tuple2<String, Double> call(Tuple2<String, Tuple2<Integer, Double>> value) throws Exception {
+                return new Tuple2<>(value._1, value._2._2);
             }
         }).mapToPair(new PairFunction<Tuple2<String, Double>, Double, String>() {
             @Override
